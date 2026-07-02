3306280bc43431dd081c98d6f1ef297bd1eea390
lawal hammed <lawalolatunde14@gmail.com>
2026-07-02 14:51:30 -0400

    spring framework exercise solution

--- src/main/java/com/amigoscode/greeting/GreetingController.java
+++ src/main/java/com/amigoscode/greeting/GreetingController.java
@@ -6,14 +6,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
+    private final GreetingService greetingService;
 
-    // TODO: 02 - Add a private final GreetingService field
-    //  Add a constructor that accepts GreetingService and assigns it to the field
-    //  Then use greetingService.greet(name) in the greet() method below
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
 
     @GetMapping("/api/v1/greetings/{name}")
     public String greet(@PathVariable String name) {
-        return "Hi, " + name + "!"; // Replace this with greetingService.greet(name)
+        return greetingService.greet(name);
     }
 
 }
--- src/main/java/com/amigoscode/greeting/GreetingService.java
+++ src/main/java/com/amigoscode/greeting/GreetingService.java
@@ -1,21 +1,36 @@
 package com.amigoscode.greeting;
 
-// TODO: 01 - Add @Component annotation to this class so Spring manages it as a bean
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
-// TODO: 03 - Add @Qualifier("greetingMessage") to the constructor parameter
 
-// TODO: 04 - Add @PostConstruct and @PreDestroy lifecycle hooks
-
+@Component
 public class GreetingService {
 
     private final String greeting;
 
-    public GreetingService(String greeting) {
+    public GreetingService(@Qualifier("greetingMessage") String greeting)
+    {
         this.greeting = greeting;
     }
 
-    public String greet(String name) {
+    @PostConstruct
+    public void init()
+    {
+        System.out.println("GreetingService has been initialized");
+    }
+
+    public String greet(String name)
+    {
         return greeting + ", " + name + "!";
+    }
+
+    @PreDestroy
+    public void destroy()
+    {
+        System.out.println("GreetingService is being destroyed");
     }
 
 }
